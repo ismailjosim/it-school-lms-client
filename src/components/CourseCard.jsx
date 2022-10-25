4067cc36289f6d5ffa909e73f3404302dbe15e57
JC DESIGN <ismailjosim99@gmail.com>
2022-10-25 22:21:14 +0600

    course details add

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const CourseCard = ({ course }) => {
-    const { name, details } = course;
+    const { name, details, picture } = course;
 
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure>
-                <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+                <img src={picture} alt="Shoes" />
             </figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const CourseDetails = () => {
+    const CourseDetailsData = useLoaderData();
+    const { name, picture, details } = CourseDetailsData;
+    console.log(CourseDetailsData);
     return (
-        <div>
-
+        <div className="p-5 mx-auto sm:p-10 md:p-16">
+            <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
+                <img src={picture} alt="course image" className="w-full h-60 sm:h-96 dark:bg-gray-500" />
+                <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-900">
+                    <div className="space-y-2">
+                        <h2 className="inline-block text-2xl font-semibold sm:text-3xl text-white">{name}</h2>
+                    </div>
+                    <div className="dark:text-gray-100">
+                        <p>{details}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
