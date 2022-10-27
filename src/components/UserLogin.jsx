eccb5da5dc67deab7f158c64b9ebcc42b8596f69
JC DESIGN <ismailjosim99@gmail.com>
2022-10-27 18:20:56 +0600

    Add password Error Message

--- src/components/UserLogin.jsx
+++ src/components/UserLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthProvider';
 import { BsGithub } from 'react-icons/bs';
@@ -10,6 +10,7 @@ import { GithubAuthProvider, GoogleAuthProvider } from 'firebase/auth';
 
 const UserLogin = () => {
     const { userSignIn, googleProviderLogin, githubProviderLogin } = useContext(AuthContext);
+    const [passwordError, setPasswordError] = useState('');
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || '/';
@@ -31,7 +32,8 @@ const UserLogin = () => {
                 navigate(from, { replace: true })
             })
             .catch(error => {
-                console.log("Error Found", error)
+                console.log("Error Found", error);
+                setPasswordError(error.message)
             });
     }
     // handle google login
@@ -57,7 +59,7 @@ const UserLogin = () => {
                 navigate(from, { replace: true })
             })
             .catch(error => {
-                console.log("Error Found", error);
+                console.log(error);
             })
     }
 
@@ -65,21 +67,22 @@ const UserLogin = () => {
     return (
         <div className='flex justify-center my-10'>
             <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-indigo-700 text-white">
+                {passwordError && <p className='text-black bg-[#F4AEA0] font-medium rounded-md'>There was a problem logging in. Check your email and password or create an account.</p>}
                 <div className="mb-8 text-center">
-                    <h1 className="my-3 text-4xl font-bold">Sign in</h1>
+                    <h2 className="my-3 text-4xl font-bold">Sign in</h2>
                 </div>
                 <form onSubmit={handleUserLogin} className="space-y-12 ng-untouched ng-pristine ng-valid">
                     <div className="space-y-4">
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm text-left">Email address</label>
-                            <input type="email" name="email" id="email" placeholder="Enter Email Address" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
+                            <input type="email" name="email" id="email" placeholder="Enter Email Address" className="w-full px-3 py-2 border rounded-md dark:border-gray-700  dark:text-gray-800" />
                         </div>
                         <div>
                             <div className="flex justify-between mb-2">
                                 <label htmlFor="password" className="text-sm">Password</label>
                                 <Link href="/" className="text-xs hover:underline dark:text-gray-400">Forgot password?</Link>
                             </div>
-                            <input type="password" name="password" id="password" placeholder="Enter Password" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
+                            <input type="password" name="password" id="password" placeholder="Enter Password" className="w-full px-3 py-2 border rounded-md dark:border-gray-700  dark:text-gray-800" />
                         </div>
                     </div>
                     <div className="space-y-2">
